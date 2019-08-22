6fce88d9760b6c974ac3ff8043719f6ba32ee7f4
Pencher zZ <nageshangzei@gmail.com>
2019-08-22 18:43:31 +0800

    Update open_sys_call_hook.c

--- c/linux-LKM/open_sys_call_hook.c
+++ c/linux-LKM/open_sys_call_hook.c
@@ -6,8 +6,41 @@
 
 MODULE_LICENSE("GPL");
 
-unsigned long **real_sys_call_table;
-asmlinkage long (*real_open)(const char __user *, int, umode_t);
+/* SMP handling */
+#define SMP_UPDATE(x) \
+	do { \
+		unsigned long __cr0; \
+		preempt_disable(); \
+		__cr0 = read_cr0() & (~X86_CR0_WP); \
+		write_cr0(__cr0); \
+		x; \
+		__cr0 = read_cr0() | X86_CR0_WP; \
+		write_cr0(__cr0); \
+		preempt_enable(); \
+	} while (0)
+
+
+static void set_addr_rw(unsigned long _addr)
+{
+	unsigned int level;
+	pte_t *pte = lookup_address(_addr, &level);
+
+	if (pte->pte &~ _PAGE_RW)
+		pte->pte |= _PAGE_RW;
+}
+
+static void set_addr_ro(unsigned long _addr)
+{
+	unsigned int level;
+	pte_t *pte = lookup_address(_addr, &level);
+
+	pte->pte = pte->pte &~_PAGE_RW;
+}
+
+
+
+unsigned long *real_sys_call_table;
+typeof(sys_open) *real_open;
 asmlinkage long fake_open(const char __user *filename, int flag, umode_t mode) {
 	if (flag & O_CREAT) {
 		printk(KERN_INFO "Open file %s.\n", filename);
@@ -15,17 +48,6 @@ asmlinkage long fake_open(const char __user *filename, int flag, umode_t mode) {
 	return real_open(filename, flag, mode);
 }
 
-static void disable_write_protect(void) {
-	unsigned long cr0 = read_cr0();
-	clear_bit(16, &cr0);
-	write_cr0(cr0);
-}
-
-static void enable_write_protect(void) {
-	unsigned long cr0 = read_cr0();
-	set_bit(16, &cr0);
-	write_cr0(cr0);
-}
 
 static unsigned long **find_sys_call_table(void) {
     unsigned long ptr;
@@ -45,19 +67,18 @@ static int __init sys_call_hook_init(void) {
 	// real_sys_call_table = find_sys_call_table();	
 	real_sys_call_table = (void *)kallsyms_lookup_name("sys_call_table");
 	printk(KERN_INFO "The system call table address is %p.\n", real_sys_call_table); 
+	real_open = (typeof(sys_open) *)real_sys_call_table[__NR_open];
+	
+	set_addr_rw((unsigned long)real_sys_call_table);
+	SMP_UPDATE({real_sys_call_table[__NR_open] = (void *)&fake_open;});
 
-	disable_write_protect();
-	real_open = (void *)real_sys_call_table[__NR_open];
-	real_sys_call_table[__NR_open] = (unsigned long *)fake_open;
-	enable_write_protect();
 
 	return 0;
 }
 
 static void __exit sys_call_hook_exit(void) {
-	disable_write_protect();
-	real_sys_call_table[__NR_open] = (unsigned long *)real_open;
-	enable_write_protect();
+	SMP_UPDATE({sys_call_table[__NR_open] = (void *)real_open;});
+	set_addr_ro((unsigned long)real_sys_call_table);
 	printk(KERN_INFO "Remove hook...\n");
 }
 
